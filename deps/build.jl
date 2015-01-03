885771c8ebdcb8e8e7aa0d49c81ca63ecb9754cf
Simon Kornblith <simon@simonster.com>
2015-01-03 18:34:07 -0500

    Update to glmnet Fortran code v5

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,3 @@
 cd(joinpath(Pkg.dir("GLMNet"), "deps"))
 pic = @windows ? "" : "-fPIC"
-run(`gfortran -m$WORD_SIZE -fdefault-real-8 -ffixed-form $pic -shared -O3 glmnet3.f90 -o libglmnet.so`)
+run(`gfortran -m$WORD_SIZE -fdefault-real-8 -ffixed-form $pic -shared -O3 glmnet5.f90 -o libglmnet.so`)
